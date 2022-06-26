93b449a89a544c185634336c89b0431eaf0128e7
Mauricio Garza Muraira <mauriciogarza4213@gmail.com>
2022-06-26 03:01:13 -0500

    Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Form from "./Components/Form/Form";
 import Posts from "./Components/Posts/Posts";
 import { useDispatch } from "react-redux";
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {HashRouter, Route, Routes} from "react-router-dom"
 import Main from "./Pages/Main"
 import { getPosts } from "./redux/actions/posts";
 import Home from "./Pages/Home";
@@ -14,12 +14,12 @@ export default function App(){
         dispatch(getPosts());
     }, [dispatch])
     return(
-        < BrowserRouter>
+        < HashRouter>
             <Routes>
                 <Route path="/main" element={<Main/>} />
                 <Route path="/form" element={<Form/>} />
                 <Route path="/" element={<Home/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
